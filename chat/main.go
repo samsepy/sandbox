4949e2868a4d123185dff06ff504c4eb0efb73a5
Hajime Todo <samsepylot@gmail.com>
2018-11-11 01:08:35 +0900

    タブをスペースに変換

--- chat/main.go
+++ chat/main.go
@@ -8,15 +8,15 @@ import (
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(`
-			<html>
-				<head>
-					<title>チャット</title>
-				</head>
-				<body>
-					チャットしましょう！
-				</body>
-			</html>
-		`))
+            <html>
+                <head>
+                    <title>チャット</title>
+                </head>
+                <body>
+                    チャットしましょう！
+                </body>
+            </html>
+        `))
 	})
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
