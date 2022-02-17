7079c8a3825670080f586a01c3bf33c5755d58dd
Hajime Todo <samsepylot@gmail.com>
2022-02-17 22:48:16 +0900

    乱数生成

--- rust/guessing_game/src/main.rs
+++ rust/guessing_game/src/main.rs
@@ -1,7 +1,15 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
+    println!("こんにちは");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    println!("秘密の数字は次の通り: {}", secret_number);
+    
     println!("数を入力してね");
+    
     let mut guess = String::new();
 
     io::stdin()
