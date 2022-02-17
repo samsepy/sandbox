1ea259f6b9314f30c03434184c71afe7719b3c2a
Hajime Todo <samsepylot@gmail.com>
2022-02-17 22:32:07 +0900

    入出力

--- rust/guessing_game/src/main.rs
+++ rust/guessing_game/src/main.rs
@@ -1,3 +1,12 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("数を入力してね");
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("読み込みに失敗しました");
+
+    println!("次のように予想しました: {}", guess)
 }
