9e9e1df2a9c4887d3d6e143e0bf607718bd1f24e
Hajime Todo <samsepylot@gmail.com>
2022-02-20 20:59:54 +0900

    ループ

--- rust/guessing_game/src/main.rs
+++ rust/guessing_game/src/main.rs
@@ -9,21 +9,23 @@ fn main() {
 
     println!("秘密の数字は次の通り: {}", secret_number);
 
-    println!("数を入力してね");
+    loop {
+        println!("数を入力してね");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("読み込みに失敗しました");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("読み込みに失敗しました");
 
-    let guess: u32 = guess.trim().parse().expect("数値を入力してください");
+        let guess: u32 = guess.trim().parse().expect("数値を入力してください");
 
-    println!("次のように予想しました: {}", guess);
+        println!("次のように予想しました: {}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("小さい！"),
-        Ordering::Greater => println!("大きい！"),
-        Ordering::Equal => println!("当たり！"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("小さい！"),
+            Ordering::Greater => println!("大きい！"),
+            Ordering::Equal => println!("当たり！"),
+        }
     }
 }
