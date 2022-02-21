980dae16fdafbb075c2e8459e2e6687de72c9ca8
Hajime Todo <samsepylot@gmail.com>
2022-02-21 22:50:51 +0900

    forループを使うことで各要素に対してコードが実行できる

--- rust/loops/src/main.rs
+++ rust/loops/src/main.rs
@@ -1,9 +1,7 @@
 fn main() {
     let a = [10, 20, 30, 40, 50];
-    let mut index = 0;
 
-    while index < 5 {
-        println!("配列{}番目のの値は{}です", index, a[index]);
-        index = index + 1;
+    for element in a.iter() {
+        println!("elementの値は{}です", element);
     }
 }
