f23cff4430b9ab22f44fdaba5f2d469d7eae149f
Hajime Todo <samsepylot@gmail.com>
2022-02-21 22:47:38 +0900

    whileを使って配列のコレクションを除きみる

--- rust/loops/src/main.rs
+++ rust/loops/src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
-    let mut number = 3;
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
 
-    while number != 0 {
-        println!("{}!", number);
-        number = number - 1;
+    while index < 5 {
+        println!("配列{}番目のの値は{}です", index, a[index]);
+        index = index + 1;
     }
-    println!("発射！");
 }
