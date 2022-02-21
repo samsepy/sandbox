b1154330325755061c1af54fa415f882156c476f
Hajime Todo <samsepylot@gmail.com>
2022-02-21 21:53:09 +0900

    関数の引数が二つ

--- rust/variables/src/main.rs
+++ rust/variables/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
-    another_function(100);
+    another_function(100, 200);
 }
 
-fn another_function(x: i32) {
-    println!("xの値は: {}", x);
+fn another_function(x: i32, y: i32) {
+    println!("xの値は: {}, yの値は: {}", x, y);
 }
