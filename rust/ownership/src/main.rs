cecfe37592d6880c56e4c23fafe9777b29b6afae
Hajime Todo <samsepylot@gmail.com>
2022-02-22 22:20:07 +0900

    タプルで複数の値を返す

--- rust/ownership/src/main.rs
+++ rust/ownership/src/main.rs
@@ -1,19 +1,13 @@
 fn main() {
-    let s1 = gives_ownership();
-    let s2 = String::from("hello");
-    let s3 = takes_and_gives_back(s2);
+    let s1 = String::from("hello");
 
-    println!("s1: {}", s1);    // OK
-    // println!("s2: {}", s2); // NG
-    println!("s3: {}", s3);    // OK
+    let (s2, len) = calculate_length(s1);
+
+    println!("The length of '{}' is {}.", s2, len);
 }
 
-fn gives_ownership() -> String {
-    let some_string = String::from("hello");
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len();
 
-    some_string
-}
-
-fn takes_and_gives_back(a_string: String) -> String {
-    a_string
+    (s, length)
 }
