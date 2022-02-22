7deb12380ce225a3a3d860e530555eab26b9a9f6
Hajime Todo <samsepylot@gmail.com>
2022-02-22 22:14:22 +0900

    ムーブいろいろ

--- rust/ownership/src/main.rs
+++ rust/ownership/src/main.rs
@@ -1,17 +1,19 @@
 fn main() {
-    let s = String::from("hello");
-    takes_ownership(s);
-    // println!("{}", s); // NG: ムーブが作用して変数sは利用不可
+    let s1 = gives_ownership();
+    let s2 = String::from("hello");
+    let s3 = takes_and_gives_back(s2);
 
-    let x = 5;
-    makes_copy(x);
-    println!("{}", x); // OK: ムーブが作用するがi32はCopyなので利用可
+    println!("s1: {}", s1);    // OK
+    // println!("s2: {}", s2); // NG
+    println!("s3: {}", s3);    // OK
+}
 
-    fn takes_ownership(some_string: String) {
-        println!("{}", some_string);
-    }
+fn gives_ownership() -> String {
+    let some_string = String::from("hello");
 
-    fn makes_copy(some_integer: i32) {
-        println!("{}", some_integer);
-    }
+    some_string
+}
+
+fn takes_and_gives_back(a_string: String) -> String {
+    a_string
 }
