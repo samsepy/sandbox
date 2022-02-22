bf850de7679fbd369761a8f0bf78b4055bb68836
Hajime Todo <samsepylot@gmail.com>
2022-02-22 22:36:55 +0900

    ムーブによるエラーの確認 re: タプルで複数の値を返す

--- rust/ownership/src/main.rs
+++ rust/ownership/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     let s1 = String::from("hello");
 
+    // let (s2, len) = (s1, s1.len()); // NG: ムーブによるエラー
     let (s2, len) = calculate_length(s1);
 
     println!("The length of '{}' is {}.", s2, len);
