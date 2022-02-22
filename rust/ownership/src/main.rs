5ccf89086e557b38334066f461c9582d72eed1dc
Hajime Todo <samsepylot@gmail.com>
2022-02-22 21:59:59 +0900

    関数におけるムーブ

--- rust/ownership/src/main.rs
+++ rust/ownership/src/main.rs
@@ -1,7 +1,17 @@
 fn main() {
-    let x = 5;
-    let mut y = x;
-    y = 10;
+    let s = String::from("hello");
+    takes_ownership(s);
+    // println!("{}", s); // NG: ムーブが作用して変数sは利用不可
 
-    println!("x = {}, y = {}", x, y);
+    let x = 5;
+    makes_copy(x);
+    println!("{}", x); // OK: ムーブが作用するがi32はCopyなので利用可
+
+    fn takes_ownership(some_string: String) {
+        println!("{}", some_string);
+    }
+
+    fn makes_copy(some_integer: i32) {
+        println!("{}", some_integer);
+    }
 }
