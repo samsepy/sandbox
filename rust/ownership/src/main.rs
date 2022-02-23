f95932891744096384e49216343d9f6e67ff1fcf
Hajime Todo <samsepylot@gmail.com>
2022-02-23 22:49:45 +0900

    as_bytesの中身を確認

--- rust/ownership/src/main.rs
+++ rust/ownership/src/main.rs
@@ -8,6 +8,9 @@ fn main() {
 fn first_word(s: &String) -> usize {
     let bytes = s.as_bytes();
 
+    // println!("bytes[0]: {}", bytes[0]); // => 104: アスキーコードでhに相当
+    // println!("bytes[0]: {}", b'h'); // => 104: hという文字リテラルからアスキーコードに変換
+
     for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
             return i;
