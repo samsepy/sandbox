2fb491f078b1c3119066ccbc086c47e91e981c8e
Hajime Todo <samsepylot@gmail.com>
2022-02-25 23:49:39 +0900

    構造体の定義とインスタンス生成

--- rust/structs/src/main.rs
+++ rust/structs/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    struct User {
+        username: String,
+        email: String,
+        sign_in_count: u64,
+        active: bool,
+    }
+
+    let user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+
+    println!("email: {}", user1.email);
+    println!("username: {}", user1.username);
+    println!("active: {}", user1.active);
+    println!("sign_in_count: {}", user1.sign_in_count);
 }
