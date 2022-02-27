1a08153c1b583665f8fbe586295d22dacc11ca26
Hajime Todo <samsepylot@gmail.com>
2022-02-27 23:55:15 +0900

    enumにメソッド定義

--- rust/enums/src/main.rs
+++ rust/enums/src/main.rs
@@ -1,13 +1,18 @@
 #[derive(Debug)]
-enum IpAddr {
-    V4(u8, u8, u8, u8),
-    V6(String),
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {
+        println!("Write: {:#?}", self);
+    }
 }
 
 fn main() {
-    let home = IpAddr::V4(127, 0, 0, 1);
-    let loopback = IpAddr::V6(String::from("::1"));
-
-    println!("あなたのIPは{:?}です", home);
-    println!("あなたのIPは{:?}です", loopback);
+    let m = Message::Write(String::from("hello"));
+    m.call();
 }
