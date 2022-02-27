e531db8cc6e128c7ab76da9776621865ebfadb0c
Hajime Todo <samsepylot@gmail.com>
2022-02-27 18:20:30 +0900

    implブロックを複数で定義してもよい

--- rust/rectangles/src/main.rs
+++ rust/rectangles/src/main.rs
@@ -8,13 +8,20 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+}
 
+impl Rectangle {
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
     }
+}
 
+impl Rectangle {
     fn square(size: u32) -> Rectangle {
-        Rectangle { width: size, height: size }
+        Rectangle {
+            width: size,
+            height: size,
+        }
     }
 }
 
