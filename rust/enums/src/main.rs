4837ab073ce226a7efce22010f8e0fff05bb2cbe
Hajime Todo <samsepylot@gmail.com>
2022-02-28 01:46:05 +0900

    Option<T>とのマッチ

--- rust/enums/src/main.rs
+++ rust/enums/src/main.rs
@@ -1,29 +1,15 @@
-#[derive(Debug)] // すぐに州を点検できるように
-enum UsState {
-    Alabama,
-    Alaska,
-}
-
-enum Coin {
-    Penny,
-    Nickel,
-    Dime,
-    Quarter(UsState),
-}
-
-fn value_in_cents(coin: Coin) -> u32 {
-    match coin {
-        Coin::Penny => 1,
-        Coin::Nickel => 5,
-        Coin::Dime => 10,
-        Coin::Quarter(state) => {
-            println!("State quarter from {:?}!", state);
-            25
-        }
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
     }
 }
 
 fn main() {
-    let c = value_in_cents(Coin::Quarter(UsState::Alabama));
-    println!("c: {}", c);
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+    println!("five: {:?}", five);
+    println!("six: {:?}", six);
+    println!("none: {:?}", none);
 }
