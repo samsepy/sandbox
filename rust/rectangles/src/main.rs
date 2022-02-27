00bafdd5e2a3aa67c7a5412bb578836d84f4e08e
Hajime Todo <samsepylot@gmail.com>
2022-02-27 17:07:56 +0900

    長方形の面積を求める

--- rust/rectangles/src/main.rs
+++ rust/rectangles/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let width1 = 30;
+    let height1 = 50;
+
+    println!("長方形の面積は{}です", area(width1, height1));
+}
+
+fn area(width: u32, height: u32) -> u32 {
+    width * height
 }
