504d5ffd6a2fe8cb3e68489d27fe086008de18b7
Hajime Todo <samsepylot@gmail.com>
2022-02-27 18:09:02 +0900

    別のRectangleインスタンスを引数にとりサイズ比較する関数追加

--- rust/rectangles/src/main.rs
+++ rust/rectangles/src/main.rs
@@ -5,18 +5,33 @@ struct Rectangle {
 }
 
 impl Rectangle {
-    fn area(&mut self) -> u32 {
-        self.width = 30 * 2;
+    fn area(&self) -> u32 {
         self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
     }
 }
 
 fn main() {
-    let mut rect1 = Rectangle {
+    let rect1 = Rectangle {
         width: 30,
         height: 50,
     };
 
+    let rect2 = Rectangle {
+        width: 10,
+        height: 10,
+    };
+
+    let rect3 = Rectangle {
+        width: 50,
+        height: 20,
+    };
+
     println!("rect1は{:#?}です", rect1);
     println!("長方形の面積は{}です", rect1.area());
+    println!("rect2はrect1にはまりますか？: {}", rect1.can_hold(&rect2));
+    println!("rect3はrect1にはまりますか？: {}", rect1.can_hold(&rect3));
 }
