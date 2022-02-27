4553baac141cf235a557e46845d81f5959922b26
Hajime Todo <samsepylot@gmail.com>
2022-02-27 19:10:03 +0900

    値をもたない状態でenumを試してみる

--- rust/enums/src/main.rs
+++ rust/enums/src/main.rs
@@ -1,3 +1,17 @@
+#[derive(PartialEq)]
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("あなたのIPは{}です", route(IpAddrKind::V4));
+}
+
+fn route(ip_type: IpAddrKind) -> String {
+    if ip_type == IpAddrKind::V4 {
+        String::from("レガシー")
+    } else {
+        String::from("モダン")
+    }
 }
