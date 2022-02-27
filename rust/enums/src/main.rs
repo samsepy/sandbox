c43b483e2dddec54abd9df32f27b974338280e70
Hajime Todo <samsepylot@gmail.com>
2022-02-27 19:54:25 +0900

    構造体を使わなずとも値を定義できる

--- rust/enums/src/main.rs
+++ rust/enums/src/main.rs
@@ -1,35 +1,13 @@
-#[derive(PartialEq)]
-enum IpAddrKind {
-    V4,
-    V6,
-}
-
-struct IpAddr {
-    kind: IpAddrKind,
-    address: String,
+#[derive(Debug)]
+enum IpAddr {
+    V4(String),
+    V6(String),
 }
 
 fn main() {
-    let home = IpAddr {
-        kind: IpAddrKind::V4,
-        address: String::from("127.0.0.1"),
-    };
+    let home = IpAddr::V4(String::from("127.0.0.1"));
+    let loopback = IpAddr::V6(String::from("::1"));
 
-    let loopback = IpAddr {
-        kind: IpAddrKind::V6,
-        address: String::from("::1"),
-    };
-
-    println!("あなたのIPは{}です", route(&home));
-    println!("あなたのIPは{}です", route(&loopback));
-}
-
-fn route(ip: &IpAddr) -> String {
-    if ip.kind == IpAddrKind::V4 {
-        let s = ip.address.to_owned() + "、" + "レガシー";
-        String::from(s)
-    } else {
-        let s = ip.address.to_owned() + "、" + "モダン";
-        String::from(s)
-    }
+    println!("あなたのIPは{:?}です", home);
+    println!("あなたのIPは{:?}です", loopback);
 }
