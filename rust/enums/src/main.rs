a0f8c9b7759d8286bbbce528e7d1d3239bd497b5
Hajime Todo <samsepylot@gmail.com>
2022-02-28 01:34:08 +0900

    enum列挙子の引数にenumを指定することでより細かなパターンマッチができる

--- rust/enums/src/main.rs
+++ rust/enums/src/main.rs
@@ -1,24 +1,29 @@
+#[derive(Debug)] // すぐに州を点検できるように
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
 }
 
 fn value_in_cents(coin: Coin) -> u32 {
-    // ifでは論理値を返す必要があるが、matchではどんな型でも良い
     match coin {
-        Coin::Penny => {
-            println!("Penny!");
-            1
-        } // アーム
-        Coin::Nickel => 5,   // アーム
-        Coin::Dime => 10,    // アーム
-        Coin::Quarter => 25, // アーム
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
     }
 }
 
 fn main() {
-    let c = value_in_cents(Coin::Penny);
+    let c = value_in_cents(Coin::Quarter(UsState::Alabama));
     println!("c: {}", c);
 }
