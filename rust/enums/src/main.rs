81e896de39aedf283348b24c7220455c294a49e7
Hajime Todo <samsepylot@gmail.com>
2022-02-27 19:29:03 +0900

    構造体を使ってenumに対する値保持を表現

--- rust/enums/src/main.rs
+++ rust/enums/src/main.rs
@@ -4,12 +4,28 @@ enum IpAddrKind {
     V6,
 }
 
-fn main() {
-    println!("あなたのIPは{}です", route(IpAddrKind::V4));
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
 }
 
-fn route(ip_type: IpAddrKind) -> String {
-    if ip_type == IpAddrKind::V4 {
+fn main() {
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+
+    println!("あなたのIPは{}、{}です", home.address, route(&home));
+    println!("あなたのIPは{}、{}です", loopback.address, route(&loopback));
+}
+
+fn route(ip: &IpAddr) -> String {
+    if ip.kind == IpAddrKind::V4 {
         String::from("レガシー")
     } else {
         String::from("モダン")
