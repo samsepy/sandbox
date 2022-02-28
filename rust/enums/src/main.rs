af09095a11449d20b4542cc75b96a1b7bcab46fc
Hajime Todo <samsepylot@gmail.com>
2022-02-28 23:23:36 +0900

    Option<T>でのマッチ

--- rust/enums/src/main.rs
+++ rust/enums/src/main.rs
@@ -1,10 +1,10 @@
 fn main() {
-    let some_u8_value = 0u8;
+    let some_u8_value = Some(0u8);
     match some_u8_value {
-        1 => println!("1"),
-        3 => println!("3"),
-        5 => println!("5"),
-        7 => println!("7"),
+        Some(1) => println!("1"),
+        Some(3) => println!("3"),
+        Some(5) => println!("5"),
+        Some(7) => println!("7"),
         _ => println!("その他"),
     }
 }
