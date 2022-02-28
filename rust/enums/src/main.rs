e15d705a220d771793654f10d2ff4ae4f188938b
Hajime Todo <samsepylot@gmail.com>
2022-02-28 23:26:58 +0900

    一つのパターンにマッチするだけでいいならif letを使うと簡素に書ける

--- rust/enums/src/main.rs
+++ rust/enums/src/main.rs
@@ -1,10 +1,8 @@
 fn main() {
     let some_u8_value = Some(0u8);
-    match some_u8_value {
-        Some(1) => println!("1"),
-        Some(3) => println!("3"),
-        Some(5) => println!("5"),
-        Some(7) => println!("7"),
-        _ => println!("その他"),
+    if let Some(3) = some_u8_value {
+        println!("3");
+    } else {
+        println!("その他");
     }
 }
