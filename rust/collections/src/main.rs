8624d2c526ac5c2fa7514856b14f469a8a1ffd62
Hajime Todo <samsepylot@gmail.com>
2022-03-02 00:10:30 +0900

    不変とか変の参照は同時に存在できない（再掲）

--- rust/collections/src/main.rs
+++ rust/collections/src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
-    let v = vec![1, 2, 3, 4, 5];
+    let mut v = vec![1, 2, 3, 4, 5];
+    let first = &v[0];
 
-    let third: &i32 = &v[2];
-    let third2: Option<&i32> = v.get(2);
-    println!("third: {}", third);
-    println!("third: {:#?}", third2);
+    // v.push(6);
+
+    println!("The first element is: {}", first);
 }
