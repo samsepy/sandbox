1861f23f0fa35140edd89303c51336a49f105831
Hajime Todo <samsepylot@gmail.com>
2022-03-02 00:21:57 +0900

    enumを使って複数の型を

--- rust/collections/src/main.rs
+++ rust/collections/src/main.rs
@@ -1,7 +1,16 @@
 fn main() {
-    let mut v = vec![100, 32, 57];
-    for i in &mut v {
-        *i += 50;
-        println!("i: {}", i)
+    #[derive(Debug)]
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
     }
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
+
+    println!("row: {:#?}", row)
 }
