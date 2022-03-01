f1963f92a4b2515da6652f3fc989793a6dbde0d0
Hajime Todo <samsepylot@gmail.com>
2022-03-02 00:19:07 +0900

    可変参照が参照している値を変更するには参照外しが必要

--- rust/collections/src/main.rs
+++ rust/collections/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
-    let v = vec![100, 32, 57];
-    for i in &v {
-        println!("{}", i);
+    let mut v = vec![100, 32, 57];
+    for i in &mut v {
+        *i += 50;
+        println!("i: {}", i)
     }
 }
