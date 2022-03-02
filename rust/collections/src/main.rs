536c74cd98eb2b39d23e80b8be486e405b668196
Hajime Todo <samsepylot@gmail.com>
2022-03-03 00:12:11 +0900

    マルチバイト文字列の場合len()の値が期待するもの結果じゃなかった

--- rust/collections/src/main.rs
+++ rust/collections/src/main.rs
@@ -1,6 +1,4 @@
 fn main() {
-    let s1 = String::from("Hello, ");
-    let s2 = String::from("world!");
-    let s3 = format!("{}{}", s1, s2);
-    println!("s3: {}", s3);
+    let s1 = String::from("Здравствуйте");
+    println!("s1: {}", s1.len());
 }
