6a32c131571a430faf1c587ed7a63a6d6a157a1f
Hajime Todo <samsepylot@gmail.com>
2022-03-03 00:01:34 +0900

    format!を使えば所有権を奪わないし可読性がある

--- rust/collections/src/main.rs
+++ rust/collections/src/main.rs
@@ -1,8 +1,6 @@
 fn main() {
     let s1 = String::from("Hello, ");
     let s2 = String::from("world!");
-    let s3 = (s1 + &s2) + &s2;
-    // println!("s1: {}", s1); // s1はムーブされ使用できない
-    println!("s2: {}", s2);
+    let s3 = format!("{}{}", s1, s2);
     println!("s3: {}", s3);
 }
