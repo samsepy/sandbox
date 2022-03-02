ecedabd40348e77150ae6035c073d0fd73424ecb
Hajime Todo <samsepylot@gmail.com>
2022-03-03 00:19:40 +0900

    文字列を走査して要素にアクセス

--- rust/collections/src/main.rs
+++ rust/collections/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     let s1 = String::from("Здравствуйте");
-    // let answer = &hello[0]; // NG: s1はマルチバイト文字列なので1バイト分のアクセスができない
-    let answer = &s1[0..2]; // NG: 1バイト分のアクセスは可能
-    println!("answer: {}", answer);
+    for c in s1.chars() {
+        println!("c: {}", c);
+    }
 }
