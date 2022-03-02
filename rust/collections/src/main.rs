fbe706286b2f7b5d95eafe3bc8d8eb53560f11a2
Hajime Todo <samsepylot@gmail.com>
2022-03-02 23:54:01 +0900

    文字列における+演算子の実験1

--- rust/collections/src/main.rs
+++ rust/collections/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     let s1 = String::from("Hello, ");
     let s2 = String::from("world!");
-    let s3 = s1 + &s2;
+    let s3 = s1 + &s2 + &s2;
     // println!("s1: {}", s1); // s1はムーブされ使用できない
     println!("s2: {}", s2);
     println!("s3: {}", s3);
