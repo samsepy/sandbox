067341a3e16824f50492d7bb93c48e617dcbfc4f
Hajime Todo <samsepylot@gmail.com>
2022-03-03 00:16:44 +0900

    s1はマルチバイト文字列なので1バイト分のアクセスができない

--- rust/collections/src/main.rs
+++ rust/collections/src/main.rs
@@ -1,4 +1,6 @@
 fn main() {
     let s1 = String::from("Здравствуйте");
-    println!("s1: {}", s1.len());
+    // let answer = &hello[0]; // NG: s1はマルチバイト文字列なので1バイト分のアクセスができない
+    let answer = &s1[0..2]; // NG: 1バイト分のアクセスは可能
+    println!("answer: {}", answer);
 }
