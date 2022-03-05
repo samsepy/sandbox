1be8f611741bb79ddcd9d7837f8bdfdaffb7baf0
Hajime Todo <samsepylot@gmail.com>
2022-03-05 23:18:20 +0900

    ハッシュマップの値にアクセス

--- rust/hashmap/src/main.rs
+++ rust/hashmap/src/main.rs
@@ -1,11 +1,13 @@
 fn main() {
     use std::collections::HashMap;
 
-    let field_name = String::from("Favorite color");
-    let field_value = String::from("Blue");
-    let mut map = HashMap::new();
-    map.insert(field_name, field_value);
-    println!("map: {:#?}", map);
-    // println!("field_name: {}", field_name); // NG: hashmapにムーブされてるため参照できない
-    // println!("field_value: {}", field_value); // NG: hashmapにムーブされてるため参照できない
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name);
+
+    println!("score: {:#?}", score);
 }
