e52e404a871289174859dde50521001e89a6dfa6
Hajime Todo <samsepylot@gmail.com>
2022-03-05 23:57:55 +0900

    古い値に基づいて値を更新する

--- rust/hashmap/src/main.rs
+++ rust/hashmap/src/main.rs
@@ -1,11 +1,15 @@
 fn main() {
     use std::collections::HashMap;
 
-    let mut scores = HashMap::new();
-    scores.insert(String::from("Blue"), 10);
+    let text = "hello world wonderful world";
 
-    scores.entry(String::from("Yellow")).or_insert(50);
-    scores.entry(String::from("Blue")).or_insert(50);
+    let mut map = HashMap::new();
 
-    println!("{:?}", scores);
+    for word in text.split_whitespace() {
+        println!("word: {}", word);
+        let count = map.entry(word).or_insert(0); // or_insert関数はこのキーに対する値への可変参照(&mut V)を返す
+        *count += 1; // 値を代入するために参照外し
+    }
+
+    println!("{:?}", map);
 }
