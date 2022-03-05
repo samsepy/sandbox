ae5d6616a4119a3d9661fe3b33f0d2d155ea2055
Hajime Todo <samsepylot@gmail.com>
2022-03-05 22:44:21 +0900

    二つベクタを使ってハッシュマップを生成する

--- rust/hashmap/src/main.rs
+++ rust/hashmap/src/main.rs
@@ -1,10 +1,9 @@
 fn main() {
     use std::collections::HashMap;
 
-    let mut scores = HashMap::new();
-
-    scores.insert(String::from("Blue"), 10);
-    scores.insert(String::from("Yellow"), 50);
+    let teams = vec![String::from("Blue"), String::from("Yellow")];
+    let initial_scores = vec![10, 50];
+    let scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
 
     println!("scores: {:#?}", scores);
 }
