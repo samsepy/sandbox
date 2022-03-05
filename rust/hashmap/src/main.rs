37818ffc079df12528c9974a6e5bd962e7e7baf8
Hajime Todo <samsepylot@gmail.com>
2022-03-05 23:05:42 +0900

    ハッシュマップと所有権

--- rust/hashmap/src/main.rs
+++ rust/hashmap/src/main.rs
@@ -1,9 +1,11 @@
 fn main() {
     use std::collections::HashMap;
 
-    let teams = vec![String::from("Blue"), String::from("Yellow")];
-    let initial_scores = vec![10, 50];
-    let scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
-
-    println!("scores: {:#?}", scores);
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+    println!("map: {:#?}", map);
+    // println!("field_name: {}", field_name); // NG: hashmapにムーブされてるため参照できない
+    // println!("field_value: {}", field_value); // NG: hashmapにムーブされてるため参照できない
 }
