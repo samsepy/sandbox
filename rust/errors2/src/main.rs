c320e3a8df26224d70ee8a0340819f36f872a9ab
Hajime Todo <samsepylot@gmail.com>
2022-03-07 23:10:23 +0900

    Resultでエラーを扱う

--- rust/errors2/src/main.rs
+++ rust/errors2/src/main.rs
@@ -2,5 +2,11 @@ use std::fs::File;
 
 fn main() {
     let f = File::open("hello.txt");
-    println!("f: {:#?}", f);
+
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => {
+            panic!("There was a problem opening the file: {:?}", error)
+        }
+    };
 }
