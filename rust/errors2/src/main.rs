a234f8ad37b709c3e098e03e86d4abf654ad9fe5
Hajime Todo <samsepylot@gmail.com>
2022-03-07 23:38:53 +0900

    マッチガードでファイルが存在しない場合はファイルを生成するように

--- rust/errors2/src/main.rs
+++ rust/errors2/src/main.rs
@@ -1,10 +1,17 @@
 use std::fs::File;
+use std::io::ErrorKind;
 
 fn main() {
     let f = File::open("hello.txt");
 
     let f = match f {
         Ok(file) => file,
+        Err(ref error) if error.kind() == ErrorKind::NotFound => match File::create("hello.txt") { // マッチガード。redはガード条件式にムーブされないように必要
+            Ok(fc) => fc,
+            Err(e) => {
+                panic!("Tried to create file but there was a problem: {:?}", e)
+            }
+        },
         Err(error) => {
             panic!("There was a problem opening the file: {:?}", error)
         }
