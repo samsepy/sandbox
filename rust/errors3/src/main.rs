2ecb66dd3e3a08e456c7e738cb29043afeb7e103
Hajime Todo <samsepylot@gmail.com>
2022-03-09 01:28:01 +0900

    数当てゲーム再

--- rust/errors3/src/main.rs
+++ rust/errors3/src/main.rs
@@ -1,3 +1,35 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("こんにちは");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    loop {
+        println!("数を入力してね");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("読み込みに失敗しました");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("次のように予想しました: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("小さい！"),
+            Ordering::Greater => println!("大きい！"),
+            Ordering::Equal => {
+                println!("当たり！");
+                break;
+            }
+        }
+    }
 }
