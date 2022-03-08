e9ea309188a8e930e31c54f07a048e8373470aaf
Hajime Todo <samsepylot@gmail.com>
2022-03-09 01:54:31 +0900

    型で1から100の値チェックを行う

--- rust/errors3/src/main.rs
+++ rust/errors3/src/main.rs
@@ -2,6 +2,24 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
+pub struct Guess {
+    value: u32,
+}
+
+impl Guess {
+    pub fn new(value: u32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess { value }
+    }
+
+    pub fn value(&self) -> u32 {
+        self.value
+    }
+}
+
 fn main() {
     println!("こんにちは");
 
@@ -16,19 +34,16 @@ fn main() {
             .read_line(&mut guess)
             .expect("読み込みに失敗しました");
 
-        let guess: i32 = match guess.trim().parse() {
+        let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
 
-        if guess < 1 || guess > 100 {
-            println!("1から100の間の数を入力してください");
-            continue;
-        }
+        let g = Guess::new(guess).value();
 
-        println!("次のように予想しました: {}", guess);
+        println!("次のように予想しました: {}", g);
 
-        match guess.cmp(&secret_number) {
+        match g.cmp(&secret_number) {
             Ordering::Less => println!("小さい！"),
             Ordering::Greater => println!("大きい！"),
             Ordering::Equal => {
