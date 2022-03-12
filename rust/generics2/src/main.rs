32acc099d16090b9c0bea50258a577b7cb22d33d
Hajime Todo <samsepylot@gmail.com>
2022-03-13 00:25:33 +0900

    ジェネリックな構造体にメソッドを定義

--- rust/generics2/src/main.rs
+++ rust/generics2/src/main.rs
@@ -4,9 +4,25 @@ struct Point<T, U> {
     y: U,
 }
 
+impl<T, U> Point<T, U> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+impl<T, U> Point<T, U> {
+    fn y(&self) -> &U {
+        &self.y
+    }
+}
+
 fn main() {
     let integer = Point { x: 5, y: 10.0 };
     let float = Point { x: 1.0, y: 4 };
     println!("integer: {:?}", integer);
     println!("float: {:?}", float);
+    println!("p.x = {}", integer.x());
+    println!("p.y = {}", integer.y());
+    println!("p.x = {}", float.x());
+    println!("p.y = {}", float.y());
 }
