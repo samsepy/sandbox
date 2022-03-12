1dedf4b22e46ce153f3e0c58745b75dd9e8536da
Hajime Todo <samsepylot@gmail.com>
2022-03-13 00:07:48 +0900

    複数のジェネリックな型引数を設定することもできる

--- rust/generics2/src/main.rs
+++ rust/generics2/src/main.rs
@@ -1,14 +1,12 @@
 #[derive(Debug)]
-struct Point<T> {
+struct Point<T, U> {
     x: T,
-    y: T,
+    y: U,
 }
 
 fn main() {
-    let integer = Point { x: 5, y: 10 };
-    // let integer = Point { x: 5, y: 10.0 }; NG: 型は同じでなくてはいけない
-    let float = Point { x: 1.0, y: 4.0 };
-    // let float = Point { x: 1.0, y: 4 }; NG: 型は同じでなくてはいけない
+    let integer = Point { x: 5, y: 10.0 };
+    let float = Point { x: 1.0, y: 4 };
     println!("integer: {:?}", integer);
     println!("float: {:?}", float);
 }
