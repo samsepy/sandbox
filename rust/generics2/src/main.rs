22d87c57dfc979b3d1aa79d9422923334ec9a664
Hajime Todo <samsepylot@gmail.com>
2022-03-13 00:01:30 +0900

    構造体の型は揃えなくてはならない

--- rust/generics2/src/main.rs
+++ rust/generics2/src/main.rs
@@ -6,7 +6,9 @@ struct Point<T> {
 
 fn main() {
     let integer = Point { x: 5, y: 10 };
+    // let integer = Point { x: 5, y: 10.0 }; NG: 型は同じでなくてはいけない
     let float = Point { x: 1.0, y: 4.0 };
+    // let float = Point { x: 1.0, y: 4 }; NG: 型は同じでなくてはいけない
     println!("integer: {:?}", integer);
     println!("float: {:?}", float);
 }
